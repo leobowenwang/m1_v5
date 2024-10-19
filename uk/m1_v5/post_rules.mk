3477a3b48c45a348e383d8f3b4411bba3bfcaf88
yangzheng <1345509173@q.com>
2024-10-19 18:13:18 +0800

    Add keyboard M1 V5

---
+++ uk/m1_v5/post_rules.mk
@@ -0,0 +1,5 @@
+RGB_MATRIX_CUSTOM_USER = yes
+include keyboards/monsgeek/m1_v5/uk/m1_v5/rgb_record/rgb_record.mk
+include keyboards/monsgeek/m1_v5/uk/m1_v5/wls/wls.mk
+include keyboards/monsgeek/wireless/wireless.mk
+
---
+++ us/m1_v5/post_rules.mk
@@ -0,0 +1,5 @@
+RGB_MATRIX_CUSTOM_USER = yes
+include keyboards/monsgeek/m1_v5/us/m1_v5/rgb_record/rgb_record.mk
+include keyboards/monsgeek/m1_v5/us/m1_v5/wls/wls.mk
+include keyboards/monsgeek/wireless/wireless.mk
+
